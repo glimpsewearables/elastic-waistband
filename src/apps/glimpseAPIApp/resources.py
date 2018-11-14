e6509adb6ff801b23d3849c3d859febebc688a75
Clayton <claytonnovotney@gmail.com>
2018-11-13 21:03:37 -0800

    Got rid of Delete everything functionality in api and allowing the api to check for a single user in the database

--- src/apps/glimpseAPIApp/resources.py
+++ src/apps/glimpseAPIApp/resources.py
@@ -4,24 +4,28 @@ from tastypie.authorization import Authorization
 
 # These files are used for the querying of data on the server side of the applications
 class UserResource(ModelResource):
+    http_method_names = ['get', 'post', 'head', 'put']
     class Meta:
         queryset = User.objects.all()
         resource_name = "user"
         authorization = Authorization()
 
 class EventResource(ModelResource):
+    http_method_names = ['get', 'post', 'head', 'put']
     class Meta:
         queryset = Event.objects.all()
         resource_name = "event"
         authorization = Authorization()
 
 class DeviceResource(ModelResource):
+    http_method_names = ['get', 'post', 'head', 'put']
     class Meta:
         queryset = Device.objects.all()
         resource_name = "device"
         authorization = Authorization()
 
 class MediaResource(ModelResource):
+    http_method_names = ['get', 'post', 'head', 'put']
     class Meta:
         queryset = Media.objects.all()
         resource_name = "media"
