a2639f301c3ff3eb3d015a2820e27c811add5f38
Clayton <Claytonnovotney@gmail.com>
2019-01-22 19:20:00 -0800

    upadted

--- src/glimpseAPI/settings.py
+++ src/glimpseAPI/settings.py
@@ -123,7 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join("static")
+# STATICFILES_DIRS = [
+#     "src/static",
+#     "src/static/",
+#     "static",
+#     "static/",
+#     "/src/static",
+#     "/src/static/",
+#     "/static",
+#     "/static/"
+# #    os.path.join(BASE_DIR, "static"),
+# ]
